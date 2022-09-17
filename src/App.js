5112c0db8848d4ebf897adaef298f11d155ba4cb
Bruno Realan <brunorealans@gmail.com>
2022-09-17 02:51:04 -0300

    Adding TailWind & DaisyUI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import CartWidget from './components/CartWidget';
 
 function App() {
   return (
     <>
-      <h1>Hola Mundo!</h1>
-      <ul>
-        <li>Numero 1</li>
-        <li>Numero 2</li>
-      </ul>
+      <CartWidget>
+        <NavBar />
+          <h1>Hola Mundo!</h1>
+          <ul>
+            <li>Numero 1</li>
+            <li>Numero 2</li>
+          </ul>
+        <Footer />
+      </CartWidget>
     </>
   );
 }
---
+++ src/components/CartWidget.js
@@ -0,0 +1,24 @@
+const CartWidget = ({ children }) => {
+    return (
+        <>
+            <div className="drawer drawer-end">
+                <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content">
+                    {/*  Page content here */}
+                    {children}
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer" className="drawer-overlay"></label>
+                    <ul className="p-4 overflow-y-auto menu w-80 bg-base-100 text-base-content">
+                        {/* Sidebar content here  */}
+                        <li><a>Sidebar Item 1</a></li>
+                        <li><a>Sidebar Item 2</a></li>
+
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default CartWidget;
