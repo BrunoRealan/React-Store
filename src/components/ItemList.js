867326848c318b8abeddea44cc9217444d929905
Bruno Realan <brunorealans@gmail.com>
2022-09-29 20:53:52 -0300

    Fixin' the render errors

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,4 +1,4 @@
-import { useEffect , useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const ItemList = () => {
@@ -10,19 +10,41 @@ const ItemList = () => {
         getProducts()
     }, [])
 
-    const getProducts = ()=> {
+    const getProducts = () => {
         const URL = "https://raw.githubusercontent.com/BrunoRealan/React-Store/main/src/resources/Products.json"
 
         fetch(URL)
             .then(response => response.json())
-            .then(data => console.log(data))
+            .then(data => setProduct(data))
     }
-    
+
     return (
         <>
-            <div>
-                {product.map (p => <img key={p.id}>{p.img}</img>)}
-            </div>
+            {product.map(p =>
+                <div className="w-full carousel" key={p.id}>
+                    <div id="slide1" className="relative w-full carousel-item">
+                        <img src={p.img} className="w-full" />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href="#slide3" className="btn btn-circle">❮</a>
+                            <a href="#slide2" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                    <div id="slide2" className="relative w-full carousel-item">
+                        <img src={p.img2} className="w-full" />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href="#slide1" className="btn btn-circle">❮</a>
+                            <a href="#slide3" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                    <div id="slide3" className="relative w-full carousel-item">
+                        <img src={p.img3} className="w-full" />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href="#slide2" className="btn btn-circle">❮</a>
+                            <a href="#slide1" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                </div>
+            )}
         </>
     )
 
